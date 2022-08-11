cf83d4fc38509f1d503a2357443c31fe57aabcfe
Éder Pires <ederpbj@yahoo.com.br>
2022-08-10 22:05:35 -0300

    baixando animação do mixamo

--- Assets/Script/CustomMovement.cs
+++ Assets/Script/CustomMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CustomMovement : MonoBehaviour
 {
+    public GameObject animCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Desabilita a camera de inicio
+        animCamera.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButton(1))
+        {
+            //Se clicar no mouse ativa camera
+            animCamera.SetActive(true);
+            print("Mouse pressionado!");
+        }
+        else
+        {
+            //O contrário fica desativada
+            animCamera.SetActive(false);
+        }
         
     }
 }
